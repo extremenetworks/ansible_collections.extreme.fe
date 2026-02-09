262a112902c67ec5212edc2d2ca2d51d75e1e406
Bjorn Haas <bhaas@extremenetworks.com>
2026-02-09 12:45:39 -0600

    Resolve warning msg

--- ansible_collections/extreme/fe/plugins/modules/extreme_fe_l3_interfaces.py
+++ ansible_collections/extreme/fe/plugins/modules/extreme_fe_l3_interfaces.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.connection import Connection, ConnectionError
-from ansible.module_utils._text import to_text
+from ansible.module_utils.common.text.converters import to_text
 
 from ipaddress import IPv4Interface, IPv6Interface, ip_network
 from typing import Dict, Iterable, List, Optional, Sequence, Set, Tuple
